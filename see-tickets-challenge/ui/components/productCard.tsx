b54aaf9fdf56cc047ff68b47ce418432ab9dfc9b
mekeirc <mekeirc@gmail.com>
2025-08-25 18:02:53 +0100

    add description page, use dynamic routes that utilises the product ID

--- see-tickets-challenge/ui/components/productCard.tsx
+++ see-tickets-challenge/ui/components/productCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ProductCard({
 	category,
@@ -21,14 +22,15 @@ export default function ProductCard({
 	title: string;
 }) {
 	return (
-		<div
+		<Link
+            href={`/description/${id}`} 
 			className="max-w-sm rounded overflow-hidden shadow-md p-5 border border-gray-200 flex flex-col align-center items-baseline justify-between"
 			key={id}
 		>
 			<div className="image-container flex align-center justify-center mx-auto mb-6">
 				<Image src={image} alt="Product Image" width={200} height={200} className="self-center" />
 			</div>
-			<h2 className="mb-4 text-1xl font-extrabold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-2xl dark:text-white">
+			<h2 className="mb-4 text-md font-extrabold leading-none tracking-tight text-gray-900 md:text-lg lg:text-xl dark:text-white">
 				{title}
 			</h2>
 			<p className="flex-1 w-full overflow-hidden description">{description}</p>
@@ -37,6 +39,6 @@ export default function ProductCard({
 				{rating.rate} / 5 <small className="text-gray-500">({rating.count} ratings)</small>
 			</p>
 			<div className="rounded-full bg-gray-100 text-blue-600 inline-flex px-3 capitalize mt-3">{category}</div>
-		</div>
+		</Link>
 	);
 }
