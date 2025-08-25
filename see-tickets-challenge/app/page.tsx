35a0bcc7b9fc5820bb920edf1d013961cdd31c8d
mekeirc <mekeirc@gmail.com>
2025-08-25 16:41:58 +0100

    cleanup

--- see-tickets-challenge/app/page.tsx
+++ see-tickets-challenge/app/page.tsx
@@ -4,23 +4,34 @@ import Nav from "../ui/components/nav";
 export default async function Page() {
 	const data = await fetch("https://fakestoreapi.com/products");
 	const posts = await data.json();
-	console.log(posts, "posts");
 
+	
 	return (
 		<div className="font-sans min-h-screen gap-16">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 				<Nav />
 				<div className="flex px-6 py-1 w-full flex-wrap gap-4 justify-center align-center">
-					{posts.map((post) => (
+					{posts.map((post: {
+						category: string;
+						description: string;
+						id: number;
+						image: string;
+						price: number;
+						rating: {
+							rate: number;
+							count: number;
+						};
+						title: string;
+					}) => (
 						<ProductCard
 							key={post.id}
-								category={post.category}
-								description={post.description}
-								id={post.id}
-								image={post.image}
-								price={post.price}
-								rating={post.rating}
-								title={post.title}
+							category={post.category}
+							description={post.description}
+							id={post.id}
+							image={post.image}
+							price={post.price}
+							rating={post.rating}
+							title={post.title}
 						/>
 					))}
 				</div>
