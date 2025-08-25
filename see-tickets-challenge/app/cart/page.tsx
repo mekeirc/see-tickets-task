f892204609ddad680eb03491b3e84b2369539b55
mekeirc <mekeirc@gmail.com>
2025-08-25 14:39:09 +0100

    set up routes, set up nav component and cart button

---
+++ see-tickets-challenge/app/cart/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">Cart</div>
+  );
+}
--- see-tickets-challenge/ui/components/nav.tsx
+++ see-tickets-challenge/ui/components/nav.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
+import CartButton from "./cartButton";
 
 export default function Nav() {
     return (
-        <div className="flex">
+        <div className="flex bg-gray-100 px-6 py-1 w-full nav justify-between">
             <Image
                 src={"https://c.ststat.net/content/seeticketsv2/eventim/svgs/see-by-eventim-white-yellow.svg"}
                 alt="Logo"
-                width={250}
+                width={150}
                 height={50}
             />
+            <CartButton />
         </div>
 	);
 }
