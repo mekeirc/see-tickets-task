8259646ba15c524e37472189451404342f404220
mekeirc <mekeirc@gmail.com>
2025-08-25 15:29:01 +0100

    set up external image host for fakestoreapi

--- see-tickets-challenge/app/page.tsx
+++ see-tickets-challenge/app/page.tsx
@@ -1,11 +1,30 @@
+import ProductCard from "@/ui/components/productCard";
 import Nav from "../ui/components/nav";
 
-export default function Home() {
-  return (
-    <div className="font-sans min-h-screen gap-16">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-          <Nav />
-      </main>
-    </div>
-  );
+export default async function Page() {
+	const data = await fetch("https://fakestoreapi.com/products");
+	const posts = await data.json();
+	console.log(posts, "posts");
+
+	return (
+		<div className="font-sans min-h-screen gap-16">
+			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+				<Nav />
+				<div className="flex px-6 py-1 w-full">
+					{posts.map((post) => (
+						<ProductCard
+							key={post.id}
+								category={post.category}
+								description={post.description}
+								id={post.id}
+								image={post.image}
+								price={post.price}
+								rating={post.rating}
+								title={post.title}
+						/>
+					))}
+				</div>
+			</main>
+		</div>
+	);
 }
